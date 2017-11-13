ac073af3eadc742eb966d81121286ff5f51a2424
aguestuser <aguestuser@riseup.net>
2017-11-12 23:50:54 -0800

    use for loop and constants in `temps.c`

--- temps.c
+++ temps.c
@@ -1,32 +1,21 @@
 # include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP  20
+
 /**
  * print Farenheit-Celsius table
  * for fahr = 0, 20, ..., 300
  **/
 
-
 int main()
 {
-  float fahr, celsius;
-  int lower, upper, step;
-
-  /* define bounds and step of table */
-  lower = 0;
-  upper = 300;
-  step = 20;
-
   /* print header */
-  printf("\nFahrenheit to Celsius:\n");
   printf("%4s | %6s\n", "fahr", "cels");
   printf("-------------\n");
 
-  /* print rows */
-  fahr = lower;
-  while (fahr <= upper) {
-    /* celsius = 5 * (fahr - 32) / 9; */
-    celsius = (5.0/9.0) * (fahr - 32.0);
-    printf("%4.0f | %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
-  }
+  int fahr;
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+    printf("%4d | %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
